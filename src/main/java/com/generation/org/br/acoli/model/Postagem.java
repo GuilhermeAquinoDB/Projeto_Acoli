e5e4b35799d7320ccc9823c9dfb58573b021024b
Mayara Amorim <101272224+Mayara-Amorim@users.noreply.github.com>
2023-03-28 10:39:58 -0300

    Camada de Security (Spring/ Basic), DTO, Usuario Model, Usuario Controller, Usuario Service e Usuario Details

--- src/main/java/com/generation/org/br/acoli/model/Postagem.java
+++ src/main/java/com/generation/org/br/acoli/model/Postagem.java
@@ -36,6 +36,11 @@ public class Postagem {
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
 	
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+	
 	public Long getId() {
 		return id;
 	}
@@ -76,6 +81,13 @@ public class Postagem {
 		this.tema = tema;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 	
 	
 }
