93968e0ac4e6af8d74b6c4bb48d5fb7e23de8e69
Lihsolive <liliamoliveira13@gmail.com>
2023-03-31 09:44:03 -0300

    Configuracao do Swagger

---
+++ src/main/java/com/generation/org/br/acoli/configuration/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package com.generation.org.br.acoli.configuration;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI springblogPessoalOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("Projeto Acoli")
+				.description( "Criação de uma rede social que conecta doadores (empresas, pessoas físicas) a pessoas carentes em situação de miséria, usando como base a ODS 2 – Erradicação da Fome.")
+				.version("v0.01")
+				.license(new License()
+						.name("Acoli")
+						.url("http://brazil.generation.org/"))
+				.contact(new Contact()
+						.name("Liliam Oliveira")
+						.url("https://github.com/Lihsolive")
+						.email("liliamsoliver@gmail.com")))
+				.externalDocs(new ExternalDocumentation().description("Github")
+						.url("https://github.com/MarcosVxny/projeto_acoli"));
+	}
+
+	@Bean
+	public OpenApiCustomizer customerGlobalHeaderOpenApiCustomiser() {
+
+		return openApi -> {
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+
+				ApiResponses apiResponses = operation.getResponses();
+
+				apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+				apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+				apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
+				apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+				apiResponses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
+				apiResponses.addApiResponse("404", createApiResponse("Objeto não encontrado!"));
+				apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+			}));
+		};
+	}
+
+	private ApiResponse createApiResponse(String message) {
+
+		return new ApiResponse().description(message);
+	}
+}
